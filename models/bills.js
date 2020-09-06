3375e69b09d958e9e378d2dc5cde1b66e49f3d0e
The Professor <professor@rem.villegascentral.com>
2020-09-06 08:29:26 -0700

    un and pw removed

--- models/bills.js
+++ models/bills.js
@@ -1,7 +1,7 @@
 exports.config={
-  user: 'bills_admin',
-  password: '4uTiliti3s tneR appLe d0Ta',
-  host: '192.168.0.32',
+  user: '',
+  password: '',
+  host: '',
   database: 'bills',
   port: 3306,
   multipleStatements: true
