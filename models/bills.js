74fb3f089bc1e71285d97dca6d44ed03a4d26469
The Professor <professor@rem.villegascentral.com>
2020-09-05 22:08:29 -0700

    gitignore added

--- models/bills.js
+++ models/bills.js
@@ -1,7 +1,7 @@
 exports.config={
-  user: '',
-  password: '',
-  host: '',
+  user: 'bills_admin',
+  password: '4uTiliti3s tneR appLe d0Ta',
+  host: '192.168.0.32',
   database: 'bills',
   port: 3306,
   multipleStatements: true
